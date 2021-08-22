56f606d8585502d9abec188a9ff0eb81a6a0086b
rafaelcsva <rafaelgyncastro63@gmail.com>
2021-08-22 18:30:32 -0300

    Add jwt on auth return

--- controller/auth.go
+++ controller/auth.go
@@ -35,5 +35,6 @@ func (u AuthController) Auth(c *gin.Context) {
 	tokenString, _ := token.SignedString([]byte(config.GetConfig().JWTKEY))
 
 	c.Header("Authorization", tokenString)
-	c.JSON(http.StatusAccepted, "OK")
+	c.JSON(http.StatusAccepted, gin.H{"Name": suser.Name,
+		"Email": suser.Email, "ImageUrl": suser.ImageUrl, "Jwt": tokenString})
 }
